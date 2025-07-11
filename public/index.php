8a341c5a3bcb6b1b7b3acd26fceaafcbe4b16fb9
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-07-11 08:36:00 +0800

    asa

--- public/index.php
+++ public/index.php
@@ -5,13 +5,13 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__ . '/../repositories/toka_baru/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . '/../repositories/toka_baru-main/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__ . '/../repositories/toka_baru/vendor/autoload.php';
+require __DIR__ . '/../repositories/toka_baru-main/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__ . '/../repositories/toka_baru/bootstrap/app.php')
+(require_once __DIR__ . '/../repositories/toka_baru-main/bootstrap/app.php')
     ->handleRequest(Request::capture());
